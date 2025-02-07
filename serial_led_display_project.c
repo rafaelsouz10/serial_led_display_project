c6a5251bda424dac0292203b47e9b76c294079cc
Rafael Souza Barbosa <rafaelsouz10@gmail.com>
2025-02-07 12:46:07 -0300

    correção nas informações do diplay oled

--- serial_led_display_project.c
+++ serial_led_display_project.c
@@ -16,27 +16,28 @@ int main() {
     
     bool cor = true;
 
-    char caractere[2] = {0}; // Garante que seja uma string válida
+    char caractere[2] = {'\0'}; // Inicializa com o caractere '-'
 
     while (true) {
         sleep_ms(50);
-        printf("\nDigite uma Caractere: ");
-        scanf("%c", &caractere[0]);
-        caractere[1] = '\0'; // Garante terminação de string
 
+        ssd1306_fill(&ssd, !cor); // Limpa o display
+        // Atualiza o conteúdo do display com animações
+        ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
+        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
+        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30); // Desenha uma string
+        ssd1306_draw_string(&ssd, caractere, 60, 48); // Desenha uma string  
+        ssd1306_send_data(&ssd); // Atualiza o display
+        
+        printf("\nDigite uma Caractere: ");
+        scanf(" %c", &caractere[0]);
+        
         if (caractere[0] != '\0') {
             printf("%c\n", caractere[0]);
-            controle_numero(caractere[0]);
-
-            cor = !cor;
-            // Atualiza o conteúdo do display com animações
-            ssd1306_fill(&ssd, !cor); // Limpa o display
-            ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
-            ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
-            ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30); // Desenha uma string
-            ssd1306_draw_string(&ssd, caractere, 60, 48); // Desenha uma string      
-            ssd1306_send_data(&ssd); // Atualiza o display
-        }
+            controle_numero(caractere[0]); 
+        } 
+        
+        
     }
     return 0;
 }
